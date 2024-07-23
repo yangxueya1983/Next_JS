b9ea43bfc9281943e560241eaf2e5be099833498
Nancy <yanxgueya1983@hotmail.com>
2024-07-22 23:41:51 -0700

    show blocks

--- src/app/blocks/page.tsx
+++ src/app/blocks/page.tsx
@@ -1,12 +1,29 @@
-import Image from "next/image";
-import Link from 'next/link'
- 
-export default function BlocksPage() {
+import { db } from "@/db";
+import Link from "next/link";
+
+export default async function BlocksPage() {
+  const blocks = await db.block.findMany();
+  const renderedBlocks = blocks.map((block) => {
+    return (
+      <Link
+        key={block.id}
+        className="flex justify-between items-center p-2 border rounded"
+        href={`/blocks/${block.id}`}
+      >
+        <div>{block.title}</div>
+        <div>View</div>
+      </Link>
+    );
+  });
   return (
     <div>
-      <h1>Hello Blocks</h1>
-      <Link href="/blocks/create">Create</Link>
+      <div className="flex m-2 justify-between items-center">
+        <h2 className="text-xl font-bold">Blocks</h2>
+        <Link href="/blocks/create" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedBlocks}</div>
     </div>
-      
   );
 }
